c8b89857f6e71607c60e29cba232637ce14aaa80
Resad <resad.hajdarpasic@gmail.com>
2019-05-17 13:06:33 +0200

    Added WorkerDao unit tests

--- src/test/java/com/hajres/domain/WorkerDaoTest.java
+++ src/test/java/com/hajres/domain/WorkerDaoTest.java
@@ -1,39 +1,73 @@
 package com.hajres.domain;
 
+import com.hajres.domain.dao.WorkerDao;
+import com.hajres.domain.model.Address;
+import com.hajres.domain.model.Company;
+import com.hajres.domain.model.Worker;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
 import java.sql.Date;
-import java.util.Properties;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkerDaoTest {
 
+
+    // Application should not add worker if address isn't set
+    // Throws SQLException
     @Test
-    public void AddingWorkerWithNullAddress() {
+    public void AddWorkerWithNullAddress() {
         WorkerDao dao = new WorkerDao();
-        Worker worker = GenerateWorkerWithoutCompanyAndAddress();
+        Worker worker = GenerateWorkerWithoutCompanyAndAddress("1111222233330");
 
         assertEquals(dao.add(worker), 0);
     }
+
+    // Application should add worker with address but no company
     @Test
-    public void AddingWorkerWithAddress(){
+    public void AddWorkerWithAddress(){
         WorkerDao dao = new WorkerDao();
-        Worker worker = GenerateWorkerWithoutCompanyAndAddress();
+        Worker worker = GenerateWorkerWithoutCompanyAndAddress("1111222233331");
         worker.setAddress(GenerateAddress());
 
         assertNotEquals(dao.add(worker), 0);
-
     }
 
-    private Worker GenerateWorkerWithoutCompanyAndAddress() {
-        Worker worker = new Worker();
+    // Application should add worker with company and address
+    @Test
+    public void AddWorkerWithAddressAndCompany() {
+        WorkerDao dao = new WorkerDao();
+        Worker worker = GenerateWorkerWithoutCompanyAndAddress("1111222233332");
+        worker.setAddress(GenerateAddress());
+        worker.setCompany(GenerateCompany());
 
-        worker.setJmbg("1234567891234");
+        assertNotEquals(dao.add(worker), 0);
+    }
+
+    @Test
+    public void FetchAllWorkerFromDatabase() {
+        WorkerDao dao = new WorkerDao();
+        ArrayList<Worker> workerList;
+        workerList = dao.findAll();
+        assertNotNull(workerList);
+    }
+
+    @Test
+    public void FetchOneWorkerFromDatabase() {
+        WorkerDao dao = new WorkerDao();
+        Worker worker = dao.findById("1111222233332");
+        assertNotNull(worker);
+    }
+
+    private Worker GenerateWorkerWithoutCompanyAndAddress(String jmbg) {
+        Worker worker = new Worker();
+        worker.setJmbg(jmbg);
         worker.setFirstName("Pero");
         worker.setLastName("Peric");
-        worker.setBirthDate(new Date(89,6, 7));
+        worker.setBirthDate(null);
 
         return worker;
     }
@@ -45,5 +79,13 @@ class WorkerDaoTest {
         address.setNumber("14b");
 
         return address;
+    }
+
+    private Company GenerateCompany() {
+        Company company = new Company();
+        company.setName("Test Company");
+        company.setAddress(GenerateAddress());
+
+        return company;
     }
 }
