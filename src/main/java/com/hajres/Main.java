66280c4b5ea45f0d1917b4396237292c760c576f
Resad <resad.hajdarpasic@gmail.com>
2019-05-20 11:58:00 +0200

    MainMenu instance is called in the main class

--- src/main/java/com/hajres/Main.java
+++ src/main/java/com/hajres/Main.java
@@ -5,11 +5,7 @@ import com.hajres.menu.MainMenu;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner;
-    private static int userInput;
-
     public static void main(String[] args) {
-        scanner = new Scanner(System.in);
         MainMenu.getInstance().start();
     }
 }
