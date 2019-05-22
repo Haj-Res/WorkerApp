3880b5902190f89366b3b756c05edf771d4a3391
Resad <resad.hajdarpasic@gmail.com>
2019-05-22 13:24:06 +0200

    Created a request switch in CompanyServlet

--- src/main/java/com/hajres/servlet/CompanyServlet.java
+++ src/main/java/com/hajres/servlet/CompanyServlet.java
@@ -1,10 +1,42 @@
-@javax.servlet.annotation.WebServlet(name = "CompanyServlet")
-public class CompanyServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.hajres.servlet;
+
+import com.hajres.domain.dao.CompanyDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/company", "/company/new", "/company/edit", "/company/delete"})
+public class CompanyServlet extends HttpServlet {
+    CompanyDao companyDao;
+
+    public void init() {
+        companyDao = new CompanyDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+        System.out.println(action);
+        switch (action) {
+            case "/company":
+                System.out.println("In /company path");
+                break;
+            case "/company/new":
+                System.out.println("In /company/new");
+                break;
+            case "/company/edit":
+                System.out.println("In /company/edit");
+                break;
+            case "/company/delete":
+                System.out.println("In /company/delete");
+                break;
+        }
     }
 }
