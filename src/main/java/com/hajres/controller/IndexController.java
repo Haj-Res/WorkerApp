553f41adecbdff389080a5af752e042f081eb841
Resad <resad.hajdarpasic@gmail.com>
2019-05-28 09:10:45 +0200

    Implemented indexController, adjusted config files for Spring MVC

--- src/main/java/com/hajres/controller/IndexController.java
+++ src/main/java/com/hajres/controller/IndexController.java
@@ -1,4 +1,15 @@
 package com.hajres.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model m) {
+        m.addAttribute("location", "index");
+        return "index";
+    }
 }
