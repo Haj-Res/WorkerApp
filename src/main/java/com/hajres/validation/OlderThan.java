7b7b846c924ac176f9f1db7081971788c5a944fc
Resad <resad.hajdarpasic@gmail.com>
2019-05-29 13:20:26 +0200

    Hopefully implemented custom age validation.

--- src/main/java/com/hajres/validation/OlderThan.java
+++ src/main/java/com/hajres/validation/OlderThan.java
@@ -1,4 +1,26 @@
 package com.hajres.validation;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = OlderThanConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface OlderThan {
+
+    // age limit
+    public int value() default 18;
+
+    // error message
+    public String message() default "must be 18 or older";
+
+    // define default groups
+    public Class<?>[] groups()  default {};
+
+    // define default payloads
+    public  Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/hajres/validation/OlderThanConstraintValidator.java
+++ src/main/java/com/hajres/validation/OlderThanConstraintValidator.java
@@ -1,4 +1,25 @@
 package com.hajres.validation;
 
-public class OlderThanConstraintValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class OlderThanConstraintValidator implements ConstraintValidator<OlderThan, Date> {
+    private int ageLimit;
+    @Override
+    public void initialize(OlderThan olderThan) {
+        ageLimit = olderThan.value();
+    }
+
+    @Override
+    public boolean isValid(Date date, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result;
+        LocalDate localDate = date.toLocalDate();
+
+        // if localDate comes after current date - age limit, it's younger than the age limit
+        // and returns false. Otherwise it returns true.
+        result = !localDate.isAfter(LocalDate.now().minusYears(ageLimit));
+        return result;
+    }
 }
