072ce19f6c55d7006d17275b7b8f88fcf87e99ae
Resad <resad.hajdarpasic@gmail.com>
2019-06-03 10:46:02 +0200

    Implemented CRUD methods in v2.WorkerDAO

--- src/main/java/com/hajres/domain/v2/dao/WorkerDAOImpl.java
+++ src/main/java/com/hajres/domain/v2/dao/WorkerDAOImpl.java
@@ -3,6 +3,7 @@ package com.hajres.domain.v2.dao;
 import com.hajres.domain.model.Worker;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,21 +17,27 @@ public class WorkerDAOImpl implements WorkerDAO {
 
     @Override
     public List<Worker> getWorkerList() {
-        return null;
+        Session session = factory.getCurrentSession();
+        return session.createQuery("from Worker ", Worker.class).getResultList();
     }
 
     @Override
     public Worker getWorker(int id) {
-        return null;
+        Session session = factory.getCurrentSession();
+        return session.get(Worker.class, id);
     }
 
     @Override
     public void saveWorker(Worker worker) {
-
+        Session session = factory.getCurrentSession();
+        session.saveOrUpdate(worker);
     }
 
     @Override
     public void deleteWorker(int id) {
-
+        Session session = factory.getCurrentSession();
+        Query query =session.createQuery("delete from Worker where id =:workerId", Worker.class);
+        query.setParameter("workerId", id);
+        query.executeUpdate();
     }
 }
