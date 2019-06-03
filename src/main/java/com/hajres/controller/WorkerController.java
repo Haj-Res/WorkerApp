b739475719039d25ee9af27b2e93d703c2112554
Resad <resad.hajdarpasic@gmail.com>
2019-06-03 13:35:41 +0200

    Updated worker controller to use workerService instead of directly using the dao objects for data

--- src/main/java/com/hajres/controller/WorkerController.java
+++ src/main/java/com/hajres/controller/WorkerController.java
@@ -3,6 +3,7 @@ package com.hajres.controller;
 import com.hajres.domain.dao.WorkerDao;
 import com.hajres.domain.model.Worker;
 
+import com.hajres.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,7 +23,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/worker")
 public class WorkerController {
-    private final WorkerDao workerDao;
+    @Autowired
+    private WorkerService workerService;
 
     @InitBinder
     public void InitBinder(WebDataBinder dataBinder) {
@@ -29,45 +32,36 @@ public class WorkerController {
         dataBinder.registerCustomEditor(String.class, trimmerEditor);
     }
 
-    @Autowired
-    public WorkerController(WorkerDao workerDao) {
-        this.workerDao = workerDao;
-    }
-
     @GetMapping("/list")
     public String showWorkerList(@ModelAttribute("filter") String filter,
                                  Model model) {
         List<Worker> workerList;
         if (filter == null) {
-            workerList = workerDao.findAll();
+            workerList = workerService.getWorkerList();
         } else {
-            workerList = workerDao.findByName(filter);
+            workerList = workerService.getWorkerList(filter);
         }
         model.addAttribute("workerList", workerList);
         return "worker/worker-list";
     }
 
     @GetMapping("/edit")
-    public String getEdit(@ModelAttribute("workerJmbg") String workerJmbg,
+    public String getEdit(@RequestParam("workerId") int workerId,
                           Model model) {
-        Worker worker = workerDao.findById(workerJmbg);
+        Worker worker = workerService.getWorker(workerId);
         model.addAttribute("worker", worker);
-        String formAction = "?workerJmbg=" + workerJmbg;
-        model.addAttribute("formAction", formAction);
         return "worker/worker-add-edit";
     }
 
-    @PostMapping("/edit")
+    @PostMapping("/save")
     public String postEdit(@Valid @ModelAttribute("worker") Worker worker,
-                           BindingResult bindingResult,
-                           @ModelAttribute("workerJmbg") String workerJmbg,
-                           Model model) {
+                           BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "worker/worker-add-edit";
         }
-        int result = workerDao.update(worker, workerJmbg);
-        prepareModel(model, result, "updated");
-        return "worker/worker-list";
+        System.out.println(worker);
+        workerService.saveWorker(worker);
+        return "redirect:/worker/list";
     }
 
     @GetMapping("/add")
@@ -79,39 +73,25 @@ public class WorkerController {
 
     @PostMapping("/add")
     public String postAdd(@Valid @ModelAttribute("worker") Worker worker,
-                          BindingResult bindingResult,
-                          Model model) {
+                          BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "worker/worker-add-edit";
         }
-        int result = workerDao.add(worker);
-        prepareModel(model, result, "added");
-        return "worker/worker-list";
+        workerService.saveWorker(worker);
+        return "redirect:/worker/list";
     }
 
     @GetMapping("/delete")
-    public String getDelete(@ModelAttribute("workerJmbg") String workerJmbg,
+    public String getDelete(@RequestParam("workerId") int workerId,
                             Model model) {
-        Worker worker = workerDao.findById(workerJmbg);
+        Worker worker = workerService.getWorker(workerId);
         model.addAttribute("worker", worker);
         return "worker/worker-delete";
     }
 
     @PostMapping("/delete")
-    public String postDelete(@ModelAttribute("workerJmbg") String workerJmbg,
-                                   Model model) {
-        int result = workerDao.delete(workerJmbg);
-        prepareModel(model, result, "deleted");
-        return "worker/worker-list";
-    }
-
-    private void prepareModel(Model model, int result, String action) {
-        if (result == 0) {
-            model.addAttribute("errorMessage", "No worker " + action + ".");
-        } else {
-            model.addAttribute("message", "Worker " + action + ".");
-        }
-        List<Worker> workerList = workerDao.findAll();
-        model.addAttribute("workerList", workerList);
+    public String postDelete(@RequestParam("workerId") int workerId) {
+        workerService.deleteWorker(workerId);
+        return "redirect:/worker/list";
     }
 }
