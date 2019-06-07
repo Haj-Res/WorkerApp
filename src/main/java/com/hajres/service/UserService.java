0576375577e821d0cea76abc27b96c57b50cad0f
Resad <resad.hajdarpasic@gmail.com>
2019-06-07 15:40:33 +0200

    Finished User and Role DAO and implementation.

--- src/main/java/com/hajres/service/UserService.java
+++ src/main/java/com/hajres/service/UserService.java
@@ -1,4 +1,11 @@
 package com.hajres.service;
 
-public interface UserService {
+import com.hajres.domain.model.User;
+import com.hajres.user.CrmUser;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    User findByUserName(String name);
+
+    void save(CrmUser user);
 }
--- src/main/java/com/hajres/service/UserServiceImpl.java
+++ src/main/java/com/hajres/service/UserServiceImpl.java
@@ -1,4 +1,68 @@
 package com.hajres.service;
 
-public class UserServiceImpl {
+import com.hajres.domain.model.Role;
+import com.hajres.domain.model.User;
+import com.hajres.domain.v2.dao.RoleDAO;
+import com.hajres.domain.v2.dao.UserDAO;
+import com.hajres.user.CrmUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private RoleDAO roleDAO;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public User findByUserName(String name) {
+        return userDAO.findByUserName(name);
+    }
+
+    @Override
+    @Transactional
+    public void save(CrmUser crmUser) {
+        User user = new User();
+
+        user.setUsername(crmUser.getUsername());
+        user.setPassword(passwordEncoder.encode(crmUser.getPassword()));
+        user.setFirstName(crmUser.getFirstName());
+        user.setLastName(crmUser.getLastName());
+        user.setEmail(crmUser.getEmail());
+
+        user.setRoles(Arrays.asList(roleDAO.findRoleByName("ROLE_EMPLOYEE")));
+
+        userDAO.save(user);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDAO.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorityies(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorityies(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
 }
