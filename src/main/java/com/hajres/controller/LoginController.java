e5f826e0b486422189710b593a6d494c75b3ee71
Resad <resad.hajdarpasic@gmail.com>
2019-06-10 10:37:27 +0200

    Added Login and Registration controller.

---
+++ src/main/java/com/hajres/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.hajres.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginFOrm() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
+    }
+}
