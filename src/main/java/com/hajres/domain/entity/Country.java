a8df6b05bbaeeaa88820ab2d474f4eb2efd35615
Resad <resad.hajdarpasic@gmail.com>
2019-06-25 11:42:37 +0200

    Added Country entity.

---
+++ src/main/java/com/hajres/domain/entity/Country.java
@@ -0,0 +1,46 @@
+package com.hajres.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    @Column(name = "country_id")
+    private String countryId;
+    @Column(name = "country_name_int")
+    private String internationalName;
+    @Column(name = "country_name_local")
+    private String localName;
+
+
+    public Country() {
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getInternationalName() {
+        return internationalName;
+    }
+
+    public void setInternationalName(String internationalName) {
+        this.internationalName = internationalName;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+}
