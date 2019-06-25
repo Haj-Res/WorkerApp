73f2bd3732f93f88cde1b3545f9189a5986d86e3
Resad <resad.hajdarpasic@gmail.com>
2019-06-25 11:17:08 +0200

    Implemented basic news controller.

--- src/main/java/com/hajres/controller/NewsController.java
+++ src/main/java/com/hajres/controller/NewsController.java
@@ -1,15 +1,27 @@
 package com.hajres.controller;
 
+import com.hajres.news.model.Article;
+import com.hajres.news.service.RestNewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/news")
 public class NewsController {
 
+    @Autowired
+    RestNewsService restNewsService;
+
     @GetMapping("")
-    public String showNews() {
+    public String getBreakingNews(Model model) {
+        List<Article> articles = restNewsService.getBreakingNews();
+        model.addAttribute("articles", articles);
         return "news/article-list";
     }
 }
